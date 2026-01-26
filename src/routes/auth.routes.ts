806d0529aa21095f453a64da124f38dd23360afe
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-01-26 22:46:43 +0800

    feat: add login with email and google routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { validateRequest } from '../middleware/validate.middleware';
 import { authenticateSupabaseUser } from '../middleware/auth.middleware';
-import { GoogleSignInSchema, RegisterSchema } from '../schemas/auth.schema';
 import {
+  GoogleSignInSchema,
+  LoginSchema,
+  RegisterSchema,
+} from '../schemas/auth.schema';
+import {
+  loginWithEmailAndPassword,
   refreshToken,
   registerWithEmailAndPassword,
   signInWithGoogle,
+  signInWithGoogleWithUserData,
 } from '../controllers/auth.controller';
 import { CreateUserFromGoogleSchema } from '../schemas/user.schema';
 import { createUser } from '../controllers/user.controller';
@@ -32,12 +38,13 @@ router.post(
 router.post('/google', validateRequest(GoogleSignInSchema), signInWithGoogle);
 
 /**
- * @route   POST /auth/google/callback
- * @desc    Handle Google OAuth callback
- * @access  Public
+ * @route   POST /auth/google/link
+ * @desc    Link Google account by creating a new user
+ * @access  Protected
  */
 router.post(
   '/google/link',
+  authenticateSupabaseUser,
   validateRequest(CreateUserFromGoogleSchema),
   createUser
 );
@@ -49,4 +56,21 @@ router.post(
  */
 router.get('/refresh', authenticateSupabaseUser, refreshToken);
 
+/**
+ * @route POST /auth/login
+ * @desc  Login user and return tokens
+ * @access Public
+ */
+router.post('/login', validateRequest(LoginSchema), loginWithEmailAndPassword);
+
+/**
+ * @route POST /auth/login/google
+ * @desc  Login user and return tokens
+ * @access Public
+ */
+router.post(
+  '/login/google',
+  validateRequest(GoogleSignInSchema),
+  signInWithGoogleWithUserData
+);
 export default router;
