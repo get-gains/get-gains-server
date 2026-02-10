ec8a181e0968a7681f3664704764c2af8642e058
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-02-11 00:46:26 +0800

    feat: increase body limit for pose detection

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,8 @@ const startServer = () => {
 
   // Middleware
   app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
   app.use((req: Request, res: Response, next: NextFunction) => {
     logPath(req.method, req.path, res.statusCode);
